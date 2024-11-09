2f2f8e1fc0e30c5e5a93272f76004cad025516e8
mario32111 <marioge44@gmail.com>
2024-11-09 16:34:30 -0700

    deploy

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes');
-const { logErrors, errorHandler,boomErrorHandler } = require ('./middlewares/error.handler')
+const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler')
 
 const app = express();
-const port = 3000;
+//con esto el puerto es dinamico, se asigna segun el puerto que heroku diga
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
+
+//solucionar el problema de CORS, y gestionar los accesos a la api
+const whitelist = ['http://localhost:8080'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin){
+      callback(null, true);
+    }else{
+      callback(new Error('no permitido'))
+    }
+  }
+};
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');

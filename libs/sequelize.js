8b4036e16eceb0a006ee0471f72a042c79e7687d
Mario <marioge44@gmail.com>
2025-01-02 02:52:59 -0600

    apply dbUrl

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -2,16 +2,23 @@ const { Sequelize } = require('sequelize');
 const {config }= require('../config/config')
 const setupModels = require('../db/models/index');
 
-
+/*
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD= encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}` */
 
 //esto ya implementa la conexion de tipo pool
-const sequelize = new Sequelize(URI , {
+const options = {
   dialect: 'postgres',
-  logging: true,
-});
+  logging: config.isProd ? false : true,
+};
+
+if (config.isProd) {
+  options.ssl = {
+    rejectUnauthorized: false,
+  };
+}
+const sequelize = new Sequelize(config.dbUrl , options);
 
 setupModels(sequelize);
 
